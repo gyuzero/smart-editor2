7274e61d089b38521385293299c2f7fd953ee5c4
gyuzero <gyuzero0103@gmail.com>
2023-09-24 17:30:22 +0900

    initial commit

---
+++ src/main/java/com/gyuzero/smarteditor2/SmartEditor2Application.java
@@ -0,0 +1,13 @@
+package com.gyuzero.smarteditor2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEditor2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartEditor2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/gyuzero/smarteditor2/SmartEditor2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyuzero.smarteditor2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEditor2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
